381998e1928cc4204782bb954f4079d3a195df54
Daniel Pelisek <dpelisek@gmail.com>
2025-11-16 21:18:30 +0100

    adding search endpoint

--- src/main/java/dp/let_me_read/api/RestApi.java
+++ src/main/java/dp/let_me_read/api/RestApi.java
@@ -1,7 +1,19 @@
 package dp.let_me_read.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@AllArgsConstructor
 @RestController
-public class RestController {
+@RequestMapping("api")
+@Slf4j
+public class RestApi {
+
+    @GetMapping("search")
+    public List<String> search(@RequestParam String query) {
+        return List.of("Pippi", "Snow white", "Ronja");
+    }
 }
